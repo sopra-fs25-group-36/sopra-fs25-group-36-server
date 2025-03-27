46a999b18ebe9b24a5327606272b7bc27a91a57f
shirleyl1220 <shirleylau27@gmail.com>
2025-03-27 19:37:08 +0100

    added requirements from lobby

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
@@ -51,6 +51,7 @@ public class LobbyController {
         return DTOMapper.INSTANCE.convertEntityToLobbyGetDTO(lobby);
     }
     
-    
+    // From Shirley ;
+    // I need the gameID to be auto created and get gameID when i do a ("/lobbies/{lobbyId}/start") mapping
 }
 

909e4745bb94ad4312d4c5aff3b3f8ed45d37c43
sing-git <seungju.paek@uzh.ch>
2025-04-01 10:42:31 +0200

    fixed StockRepo error

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/StockRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/StockRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
-    // All stock records for a given symbol 
+    // All stock records for a given symbol
     List<Stock> findBySymbol(String symbol);
 
     // A specific day's record for a stock symbol
@@ -18,9 +18,6 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 
     // Records for all stocks on a given date
     List<Stock> findAllByDate(LocalDate date);
-
-    // Specific source + symbol
-    List<Stock> findBySymbolAndSource(String symbol, String source);
 
     // Get all prices for a symbol in a time range
     List<Stock> findBySymbolAndDateBetween(String symbol, LocalDate start, LocalDate end);

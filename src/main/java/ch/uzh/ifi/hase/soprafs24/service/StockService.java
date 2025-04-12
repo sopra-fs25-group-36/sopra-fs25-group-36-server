fb2d9c5dab3b2184293f746d285fd206a9f420f4
JuliusLhamo <juliuslhamolandes@gmail.com>
2025-04-12 09:01:35 +0200

    quick fix StockService.java imports

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/StockService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/StockService.java
@@ -6,6 +6,15 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;

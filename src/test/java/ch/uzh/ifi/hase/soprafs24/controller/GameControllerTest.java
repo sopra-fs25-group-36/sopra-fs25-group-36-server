0b67a71900f83164c808cdb4d8b4bb444f7312b7
sing-git <seungju.paek@uzh.ch>
2025-05-15 01:53:05 +0200

    add more test for gamecontroller

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/GameControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/GameControllerTest.java
@@ -8,13 +8,21 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import ch.uzh.ifi.hase.soprafs24.entity.Game;
+import ch.uzh.ifi.hase.soprafs24.game.GameManager;
 import ch.uzh.ifi.hase.soprafs24.service.GameService;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(GameController.class)
 public class GameControllerTest {
@@ -42,4 +50,21 @@ public class GameControllerTest {
                 .andExpect(jsonPath("$.id", is(game.getId().intValue())));
 
     }
-}
+
+     @Test
+    public void getGame_validInput() throws Exception {
+        // given
+        Long gameId = 188L;
+        GameManager gameManager = new GameManager(gameId, new LinkedHashMap<>(), 50);
+        given(gameService.getGame(gameId)).willReturn(gameManager);
+
+        // when/then -> do the request + validate the result
+        MockHttpServletRequestBuilder getRequest = get("/game/{gameId}", gameId);
+
+        // then
+        mockMvc.perform(getRequest)
+                .andExpect(status().isOk());    }
+
+     }
+
+
